977d1447ecfa0e7c642ba87eb8070ee758ea356a
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-07 20:44:10 +0800

    Cool logo and shit

--- SwiftUIPlayground/Views/AnotherReallyAwesomeView.swift
+++ SwiftUIPlayground/Views/AnotherReallyAwesomeView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct AnotherReallyAwesomeView: View {
     var body: some View {
-        Text("AnotherReallyAwesomeView")
+        Text("f")
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .aspectRatio(1.0, contentMode: .fit)
+            .padding()
+            .background(Color(#colorLiteral(red: 0.2358980775, green: 0.3523842096, blue: 0.5996781588, alpha: 1)))
+            .clipShape(Circle())
+            .padding(2)
+            .background(Circle())
     }
 }
 
--- SwiftUIPlayground/Views/SomeReallyAwesomeView.swift
+++ SwiftUIPlayground/Views/SomeReallyAwesomeView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct SomeReallyAwesomeView: View {
     var body: some View {
-        Text("SomeReallyAwesomeView")
+        Text("t")
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .aspectRatio(1.0, contentMode: .fit)
+            .padding()
+            .background(Color(#colorLiteral(red: 0.2568539977, green: 0.7132608294, blue: 0.9055070281, alpha: 1)))
+            .clipShape(Circle())
+            .padding(2)
+            .background(Circle())
     }
 }
 
