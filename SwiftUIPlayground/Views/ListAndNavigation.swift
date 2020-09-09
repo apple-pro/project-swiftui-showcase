84f99af182a241ec02d432e030cc652c738ffaf9
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-09 19:02:39 +0800

    Initial List

--- SwiftUIPlayground/Views/ListAndNavigation.swift
+++ SwiftUIPlayground/Views/ListAndNavigation.swift
@@ -7,65 +7,43 @@
 //
 
 import SwiftUI
-import MapKit
+
 
 struct ListAndNavigation: View {
     
     var body: some View {
-        VStack {
-            MapView()
-                .edgesIgnoringSafeArea(.top)
-                .frame(height: 300)
+        List(landmarkData, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+        .navigationBarTitle(Text("Landmarks"))
+    }
+}
 
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
 
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-
-                HStack(alignment: .top) {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-            }
-            .padding()
-
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
             Spacer()
         }
-    }
-}
-
-struct MapView: UIViewRepresentable {
-    func makeUIView(context: Context) -> MKMapView {
-        MKMapView(frame: .zero)
-    }
-
-    func updateUIView(_ uiView: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011_286, longitude: -116.166_868)
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        uiView.setRegion(region, animated: true)
-    }
-}
-
-struct CircleImage: View {
-    var body: some View {
-        Image("turtlerock")
-            .clipShape(Circle())
-            .overlay(
-                Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: 10)
     }
 }
 
 struct GridSearch_Previews: PreviewProvider {
     static var previews: some View {
         ListAndNavigation()
+        //LandmarkRow(landmark: landmarkData[2])
+        
+//        Group {
+//            LandmarkRow(landmark: landmarkData[0])
+//                .previewLayout(.fixed(width: 300, height: 70))
+//            LandmarkRow(landmark: landmarkData[1])
+//                .previewLayout(.fixed(width: 300, height: 70))
+//        }
     }
 }
---
+++ SwiftUIPlayground/Views/ListAndNavigation/LandmarkDetailView.swift
@@ -0,0 +1,70 @@
+//
+//  LandmarkDetailView.swift
+//  SwiftUIPlayground
+//
+//  Created by StartupBuilder.INFO on 9/9/20.
+//  Copyright © 2020 StartupBuilder.INFO. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct LandmarkDetailView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011_286, longitude: -116.166_868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct LandmarkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetailView()
+    }
+}
